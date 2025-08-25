44e8423621aafe37836c4076c78763e03997fa36
3xter666 <23520348@gm.uit.edu.vn>
2025-08-25 15:19:49 +0000

    H: Add custom CSS styles for improved aesthetics and readability
    - Set body background color and text color for a dark theme
    - Define styles for headings (h1-h6) with specific font sizes and text shadows
    - Style links with hover effects and custom colors
    - Enhance code blocks and preformatted text with background colors and padding
    - Style blockquotes for better visual separation
    - Add responsive image styles with rounded corners and shadows
    - Implement custom scrollbar styles for a cohesive look

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-from textnode import *
-print("hello world")
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
 def main():
-    new_text = TextNode("Hello, world!", TextType.TEXT, "https://www.chat.com")
-    text = new_text.__repr__()
-    print(text)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+
 main()
